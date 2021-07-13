fb8b4b4559bb460367900e478fb10fa0187f5bed
Muhammad Razif Rizqullah <razifrizqullah@gmail.com>
2021-07-13 16:31:59 +0700

    register and login page view OK

--- UserAuthApp/Program.cs
+++ UserAuthApp/Program.cs
@@ -8,7 +8,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Register {
+namespace UserAuth {
   public class Program {
     public static async Task Main(string[] args) {
       var builder = WebAssemblyHostBuilder.CreateDefault(args);
