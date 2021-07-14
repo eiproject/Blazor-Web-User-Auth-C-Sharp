45a6901ff23caa798b0aa769591a820829018866
Muhammad Razif Rizqullah <razifrizqullah@gmail.com>
2021-07-14 10:08:24 +0700

    adding hash to password

--- UserAuthApp/Models/User.cs
+++ UserAuthApp/Models/User.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace UserAuth.Models {
@@ -9,13 +11,29 @@ namespace UserAuth.Models {
     private string _password;
     internal User() { }
     void IUser.SetUser(string email, string password) {
-      _email = email; _password = password;
+      _email = email; 
+      _password = ComputeSha256Hash(password);
     }
     string IUser.GetEmail() {
       return _email;
     }
     string IUser.GetPassword() {
       return _password;
+    }
+    // Hashing SHA256
+    string ComputeSha256Hash(string rawData) {
+      // Create a SHA256   
+      using (SHA256 sha256Hash = SHA256.Create()) {
+        // ComputeHash - returns byte array  
+        byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+        // Convert byte array to a string   
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < bytes.Length; i++) {
+          builder.Append(bytes[i].ToString("x2"));
+        }
+        return builder.ToString();
+      }
     }
   }
 }
